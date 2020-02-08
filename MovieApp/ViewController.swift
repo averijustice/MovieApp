81f0d38fe8c61e29658b4f3d4e71e0a1af1e929f
Averi Justice <justic35@msu.edu>
2020-02-07 21:00:31 -0500

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Averi Justice on 2/7/20.
+//  Copyright © 2020 aaejustice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
